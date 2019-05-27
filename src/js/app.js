61cb29f3df2d2093d5b19ff1704383ffa124051f
amtlib <grzegorz.pach@protonmail.com>
2019-05-27 11:00:20 +0200

    Add smooth scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import SmoothScroll from 'smooth-scroll'
 import 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css'
 import '../css/style.scss'
@@ -6,4 +7,7 @@ import '../css/style.scss'
 $(document).ready(() => {
     let trigger = document.querySelector('.sidenav')
     let instance = M.Sidenav.init(trigger)
+
+    //smooth scroll
+    new SmoothScroll('a[href*="#"]')
 })
